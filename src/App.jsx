2a2b8b89e69dabecc704352d2b9936d055e61906
Beabfekad Zikie <92110044+beabzk@users.noreply.github.com>
2024-09-23 17:43:47 +0300

    feat: Add initial project files and configuration

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>YAMDS: Yet Another Movie Database Searcher</h1>
+    </>
+  );
+}
+
+export default App;
