6d1c080bb239e9c001a62bc6e438d6a715ac184f
Beabfekad Zikie <92110044+beabzk@users.noreply.github.com>
2024-09-23 19:44:58 +0300

    feat: Add MovieCard and MovieList components

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import MovieCard from "./MovieCard";
 
-function MovieList() {
-  return <div>MovieList</div>;
-}
+const MovieList = ({ movies }) => {
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4">
+      {movies.map((movie) => (
+        <MovieCard key={movie.id} movie={movie} />
+      ))}
+    </div>
+  );
+};
 
 export default MovieList;
